27792034841afe798e2571533beeb08b637ff954
OutstandingMove <benny_p1337@hotmail.com>
2022-11-18 13:24:37 +1100

    To do List - MVC

--- todoList/main.go
+++ todoList/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/BenPhamily/GetGoing/todoList/structs"
+	"github.com/BenPhamily/GetGoing/todoList/controller"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			data := structs.Response{
-				Code: http.StatusOK,
-				Body: "pong",
-			}
-			json.NewEncoder(w).Encode(data)
-		}
-	})
-
+	mux := controller.Register()
 	http.ListenAndServe(":3000", mux)
 }
--- todoList/structs/structs.go
+++
@@ -1,8 +0,0 @@
-package structs
-
-type Response struct {
-	Code int
-	Body interface{}
-	// Code int         `json:"code`
-	// Body interface{} `json:"body`
-}
